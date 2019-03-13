da2c761aebc48db5464212ffd2a18ca53d46a8a1
Tyler Akins <fidian@rumkin.com>
2019-03-13 11:59:45 -0500

    Linux does not use a wait union.

--- sh.proc.c
+++ sh.proc.c
@@ -51,7 +51,7 @@ RCSID("$Id: sh.proc.c,v 3.74 2001/06/10 02:26:38 christos Exp $")
 # define HZ 16
 #endif /* aiws */
 
-#if defined(_BSD) || (defined(IRIS4D) && __STDC__) || defined(__lucid) || defined(linux)
+#if defined(_BSD) || (defined(IRIS4D) && __STDC__) || defined(__lucid)
 # define BSDWAIT
 #endif /* _BSD || (IRIS4D && __STDC__) || __lucid || linux */
 #ifndef WTERMSIG
