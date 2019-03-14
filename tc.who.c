a2cf9c4d251b154ad019092e5d440e7d2d85d88b
Tyler Akins <fidian@rumkin.com>
2019-03-14 08:02:16 -0500

    6.12.01

--- tc.who.c
+++ tc.who.c
@@ -1,4 +1,4 @@
-/* $Header: /src/pub/tcsh/tc.who.c,v 3.34 2002/06/25 19:02:11 christos Exp $ */
+/* $Header: /src/pub/tcsh/tc.who.c,v 3.36 2002/09/15 16:58:52 christos Exp $ */
 /*
  * tc.who.c: Watch logins and logouts...
  */
@@ -32,7 +32,7 @@
  */
 #include "sh.h"
 
-RCSID("$Id: tc.who.c,v 3.34 2002/06/25 19:02:11 christos Exp $")
+RCSID("$Id: tc.who.c,v 3.36 2002/09/15 16:58:52 christos Exp $")
 
 #include "tc.h"
 
@@ -52,6 +52,8 @@ RCSID("$Id: tc.who.c,v 3.34 2002/06/25 19:02:11 christos Exp $")
 # ifndef _PATH_UTMP
 #  if defined(__UTMPX_FILE) && !defined(UTMPX_FILE)
 #   define _PATH_UTMP __UTMPX_FILE
+#  elif defined(_PATH_UTMPX)
+#   define _PATH_UTMP _PATH_UTMPX
 #  else
 #   define _PATH_UTMP UTMPX_FILE
 #  endif /* __UTMPX_FILE && !UTMPX_FILE */
@@ -261,7 +263,7 @@ watch_login(force)
 	return;
     }
     stlast = sta.st_mtime;
-    if ((utmpfd = open(_PATH_UTMP, O_RDONLY)) < 0) {
+    if ((utmpfd = open(_PATH_UTMP, O_RDONLY|O_LARGEFILE)) < 0) {
 	if (!force)
 	    xprintf(CGETS(26, 2,
 			  "%s cannot be opened.  Please \"unset watch\".\n"),
