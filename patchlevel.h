1fc5b4ffc3042cdb16c3484d41f5fbda084fc561
Tyler Akins <fidian@rumkin.com>
2019-03-14 08:02:03 -0500

    6.06.03

--- patchlevel.h
+++ patchlevel.h
@@ -1,14 +1,14 @@
-/* $Header: /u/christos/cvsroot/tcsh/patchlevel.h,v 3.95 1996/04/26 19:18:41 christos Exp $ */
+/* $Header: /u/christos/cvsroot/tcsh/patchlevel.h,v 3.97 1996/09/24 16:57:23 christos Exp $ */
 /*
  * patchlevel.h: Our life story.
  */
 #ifndef _h_patchlevel
 #define _h_patchlevel
 
-#define ORIGIN "D.E.Shaw"
+#define ORIGIN "Astron"
 #define REV 6
 #define VERS 6
-#define PATCHLEVEL 2
-#define DATE "1996-06-22"
+#define PATCHLEVEL 3
+#define DATE "1996-09-24"
 
 #endif /* _h_patchlevel */
